64ff9d595ff732e31f867ad11ff387cdd4e5bcad
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-07-17 22:54:34 -0500

    feat: ✨ Implement Endpoints of classes table
    Implement the get/post/put/delete endopoints of classes table

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 import "reflect-metadata";
 import teachersHub from "./routers/teachers.js";
 import studentsHub from "./routers/students.js";
+import classesHub from "./routers/classes.js";
 const appExpress = express();
 dotenv.config();
 
 appExpress.use(express.json());
 appExpress.use('/teachers', teachersHub);
-appExpress.use('/students', studentsHub)
+appExpress.use('/students', studentsHub);
+appExpress.use('/classes', classesHub)
 
 const config = JSON.parse(process.env.MY_CONFIG);
 appExpress.listen(config, ()=>{
---
+++ routers/classes.js
@@ -0,0 +1,92 @@
+import mysql from "mysql2";
+import { Router } from "express";
+
+const classesHub = Router();
+let connection;
+
+classesHub.use((req, res, next) => {
+  const config = JSON.parse(process.env.MY_CONNECT)
+  connection = mysql.createPool(config);
+  next();
+})
+
+classesHub.get("/", (req, res) => {
+  connection.query('SELECT * FROM classes', (err, result, fil) => {
+    res.end(JSON.stringify(result));
+  })
+})
+
+classesHub.get("/:id", (req, res) => {
+  const classId = req.params.id;
+
+  connection.query(
+    "SELECT * FROM classes WHERE class_id = ?",
+    [classId],
+    (err, result) => {
+      if (err) {
+        console.error("¡ERROR! Error Getting Class Information", err);
+        return res.status(500).json({ mensaje: "¡ERROR! Error Getting Class Information" });
+      }
+
+      if (result.length === 0) {
+        return res.status(404).json({ mensaje: "¡ERROR! No Class Information Found" });
+      }
+
+      const Class = result[0];
+      return res.json(Class);
+    }
+  );
+});
+
+classesHub.post("/", (req, res) => {
+  const { id, name, teacher_id } = req.body;
+
+  connection.query(
+    'INSERT INTO classes(class_id, class_name, class_teacher_id) VALUES (?,?,?)',
+    [ id, name, teacher_id],
+    (err, result, fields) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("¡ERROR! Error Inserting Data Into the DataBase");
+      } else {
+        res.end("The Data Was Successfully Inserted Into The DataBase :)");
+      }
+    });
+});
+
+classesHub.put("/:id", (req, res) => {
+  const classId = req.params.id;
+  const {name, teacher_id} = req.body;
+
+  connection.query(
+    "UPDATE classes SET class_name = ?, class_teacher_id = ? WHERE class_id = ?",
+    [ name, teacher_id, classId],
+    (err, result) => {
+      if (err) {
+        console.error("Failed To Update Class's Information", err);
+        return res.status(500).json({ mensaje: "Failed To Update Class's Information" });
+      }
+
+      return res.json({ mensaje: "Class Information Successfully Updated :)" });
+    }
+  );
+});
+
+classesHub.delete("/:id", (req, res) => {
+  const classId = req.params.id;
+
+  connection.query(
+    "DELETE FROM classes WHERE class_id = ?",
+    [classId],
+    (err, result) => {
+      if (err) {
+        console.error("Error Deleting Class's Information: ", err);
+        return res.status(500).json({ mensaje: "Error Deleting Class's Information" });
+      }
+
+      return res.json({ mensaje: "Class's Information Successfully deleted" });
+    }
+  );
+});
+
+export default classesHub;
