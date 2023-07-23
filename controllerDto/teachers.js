482d409e1ab74fc8be7e2ff7e5065595c21c3dc3
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-07-23 10:26:54 -0500

    feat: ✨ Implement NEW DTO of teachers table
    Implement a new DTO method with typescript for teachers table

--- controllerDto/teachers.js
+++ controllerDto/teachers.js
@@ -8,23 +8,21 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 import { Expose, Type } from 'class-transformer';
-import { IsDefined, MaxLength, IsEnum, IsEmail, IsString, IsInt, Matches } from "class-validator";
-var Gender;
-(function (Gender) {
-    Gender["Male"] = "Male";
-    Gender["Female"] = "Female";
-    Gender["Other"] = "Other";
-})(Gender || (Gender = {}));
+import { IsDefined, MaxLength, IsEmail, IsString, IsInt, Matches } from "class-validator";
 export class teachers {
-    constructor(teacher_id, teacher_name, teacher_specialty, teacher_phone, teacher_age, teacher_gender, teacher_email, teacher_address) {
+    constructor(teacher_id, teacher_name, teacher_gender, teacher_age, teacher_address, teacher_phone, teacher_phone_type, teacher_email, teacher_email_type, teacher_specialty_id, teacher_class_id, teacher_roll_id) {
         this.teacher_id = teacher_id;
         this.teacher_name = teacher_name;
-        this.teacher_specialty = teacher_specialty;
-        this.teacher_phone = teacher_phone;
-        this.teacher_age = teacher_age;
         this.teacher_gender = teacher_gender;
-        this.teacher_email = teacher_email;
+        this.teacher_age = teacher_age;
         this.teacher_address = teacher_address;
+        this.teacher_phone = teacher_phone;
+        this.teacher_phone_type = teacher_phone_type;
+        this.teacher_email = teacher_email;
+        this.teacher_email_type = teacher_email_type;
+        this.teacher_specialty_id = teacher_specialty_id;
+        this.teacher_class_id = teacher_class_id;
+        this.teacher_roll_id = teacher_roll_id;
     }
 }
 __decorate([
@@ -44,22 +42,12 @@ __decorate([
     __metadata("design:type", String)
 ], teachers.prototype, "teacher_name", void 0);
 __decorate([
-    Expose({ name: 'specialty' }),
-    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Specialty" parameter is required >:(' }; } }),
-    IsString({ message: () => { throw { status: 401, message: '¡ERROR! The "Specialty" parameter does not comply with the established data type >:(' }; } }),
-    MaxLength(50, { message: () => { throw { status: 401, message: '¡ERROR! The "Specialty" parameter has exceeded the limit of characters >:(' }; } }),
-    Matches(/^[A-Za-zñÑáéíóúÁÉÍÓÚ\s]+$/, { message: () => { throw { status: 401, message: '¡ERROR! The "Specialty" parameter contains invalid characters >:(' }; } }),
-    Type(() => String),
-    __metadata("design:type", String)
-], teachers.prototype, "teacher_specialty", void 0);
-__decorate([
-    Expose({ name: 'phone' }),
-    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Phone" parameter is required >:(' }; } }),
-    IsString({ message: () => { throw { status: 401, message: '¡ERROR! The "Phone" parameter does not comply with the established data type >:(' }; } }),
-    MaxLength(15, { message: () => { throw { status: 401, message: '¡ERROR! The "Phone" parameter has exceeded the limit of characters >:(' }; } }),
-    Type(() => String),
-    __metadata("design:type", String)
-], teachers.prototype, "teacher_phone", void 0);
+    Expose({ name: 'gender' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Gender" parameter is required >:(' }; } }),
+    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! The "Gender" parameter does not comply with the established data type >:(' }; } }),
+    Type(() => Number),
+    __metadata("design:type", Number)
+], teachers.prototype, "teacher_gender", void 0);
 __decorate([
     Expose({ name: 'age' }),
     IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Age" parameter is required >:(' }; } }),
@@ -67,18 +55,6 @@ __decorate([
     Type(() => Number),
     __metadata("design:type", Number)
 ], teachers.prototype, "teacher_age", void 0);
-__decorate([
-    Expose({ name: 'gender' }),
-    IsEnum(Gender, { message: () => { throw { status: 401, message: '¡ERROR! The "Gender" parameter does not comply with the established data type >:(' }; } }),
-    Type(() => String),
-    __metadata("design:type", String)
-], teachers.prototype, "teacher_gender", void 0);
-__decorate([
-    Expose({ name: 'email' }),
-    IsEmail({}, { message: () => { throw { status: 401, message: '¡ERROR! The "Email" parameter does not comply with the established data type >:(' }; } }),
-    Type(() => String),
-    __metadata("design:type", String)
-], teachers.prototype, "teacher_email", void 0);
 __decorate([
     Expose({ name: 'address' }),
     IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Address" parameter is required >:(' }; } }),
@@ -88,3 +64,54 @@ __decorate([
     Type(() => String),
     __metadata("design:type", String)
 ], teachers.prototype, "teacher_address", void 0);
+__decorate([
+    Expose({ name: 'phone' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Phone" parameter is required >:(' }; } }),
+    IsString({ message: () => { throw { status: 401, message: '¡ERROR! The "Phone" parameter does not comply with the established data type >:(' }; } }),
+    MaxLength(15, { message: () => { throw { status: 401, message: '¡ERROR! The "Phone" parameter has exceeded the limit of characters >:(' }; } }),
+    Type(() => String),
+    __metadata("design:type", String)
+], teachers.prototype, "teacher_phone", void 0);
+__decorate([
+    Expose({ name: 'phone_type' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Phone_Type" parameter is required >:(' }; } }),
+    IsString({ message: () => { throw { status: 401, message: '¡ERROR! The "Phone_Type" parameter does not comply with the established data type >:(' }; } }),
+    MaxLength(15, { message: () => { throw { status: 401, message: '¡ERROR! The "Phone_Type" parameter has exceeded the limit of characters >:(' }; } }),
+    Type(() => String),
+    __metadata("design:type", String)
+], teachers.prototype, "teacher_phone_type", void 0);
+__decorate([
+    Expose({ name: 'email' }),
+    IsEmail({}, { message: () => { throw { status: 401, message: '¡ERROR! The "Email" parameter does not comply with the established data type >:(' }; } }),
+    Type(() => String),
+    __metadata("design:type", String)
+], teachers.prototype, "teacher_email", void 0);
+__decorate([
+    Expose({ name: 'email_type' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Email_Type" parameter is required >:(' }; } }),
+    IsString({ message: () => { throw { status: 401, message: '¡ERROR! The "Email_Type" parameter does not comply with the established data type >:(' }; } }),
+    MaxLength(15, { message: () => { throw { status: 401, message: '¡ERROR! The "Email_Type" parameter has exceeded the limit of characters >:(' }; } }),
+    Type(() => String),
+    __metadata("design:type", String)
+], teachers.prototype, "teacher_email_type", void 0);
+__decorate([
+    Expose({ name: 'specialty_id' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Age" parameter is required >:(' }; } }),
+    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! The "Age" parameter does not comply with the established data type >:(' }; } }),
+    Type(() => Number),
+    __metadata("design:type", Number)
+], teachers.prototype, "teacher_specialty_id", void 0);
+__decorate([
+    Expose({ name: 'class_id' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Age" parameter is required >:(' }; } }),
+    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! The "Age" parameter does not comply with the established data type >:(' }; } }),
+    Type(() => Number),
+    __metadata("design:type", Number)
+], teachers.prototype, "teacher_class_id", void 0);
+__decorate([
+    Expose({ name: 'roll_id' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Age" parameter is required >:(' }; } }),
+    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! The "Age" parameter does not comply with the established data type >:(' }; } }),
+    Type(() => Number),
+    __metadata("design:type", Number)
+], teachers.prototype, "teacher_roll_id", void 0);
--- dto/teachers.ts
+++ dto/teachers.ts
@@ -1,11 +1,5 @@
 import { Expose, Type} from 'class-transformer';
-import { IsDefined, MaxLength, IsEnum, IsEmail, IsString, IsInt, Matches} from "class-validator";
-
-enum Gender {
-    Male = 'Male',
-    Female = 'Female',
-    Other = 'Other',
-}
+import { IsDefined, MaxLength, IsEmail, IsString, IsInt, Matches} from "class-validator";
   
 export class teachers {
 
@@ -23,36 +17,17 @@ export class teachers {
     @Type(()=>String)
     teacher_name : string;
 
-    @Expose({ name: 'specialty' })
-    @IsDefined({message: ()=>{throw {status:401, message:'¡ERROR! The "Specialty" parameter is required >:('}}})
-    @IsString({message: ()=>{throw {status:401, message: '¡ERROR! The "Specialty" parameter does not comply with the established data type >:('}}})
-    @MaxLength(50,{message: ()=>{throw {status:401, message:'¡ERROR! The "Specialty" parameter has exceeded the limit of characters >:('}}})
-    @Matches(/^[A-Za-zñÑáéíóúÁÉÍÓÚ\s]+$/, {message: ()=>{throw{status:401, message:'¡ERROR! The "Specialty" parameter contains invalid characters >:('}}})
-    @Type(()=>String)
-    teacher_specialty : string;
-
-    @Expose({ name: 'phone' })
-    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Phone" parameter is required >:('}}})
-    @IsString({message: ()=>{throw {status:401, message: '¡ERROR! The "Phone" parameter does not comply with the established data type >:('}}})
-    @MaxLength(15,{message: ()=>{throw {status:401, message:'¡ERROR! The "Phone" parameter has exceeded the limit of characters >:('}}})
-    @Type(()=>String)
-    teacher_phone: string;
+    @Expose({ name: 'gender' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Gender" parameter is required >:('}}})
+    @IsInt({message: ()=>{throw {status:401, message: '¡ERROR! The "Gender" parameter does not comply with the established data type >:('}}})
+    @Type(()=>Number)
+    teacher_gender: number;
 
     @Expose({ name: 'age' })
     @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Age" parameter is required >:('}}})
     @IsInt({message: ()=>{throw {status:401, message: '¡ERROR! The "Age" parameter does not comply with the established data type >:('}}})
     @Type(()=>Number)
     teacher_age: number;
-
-    @Expose({ name: 'gender' })
-    @IsEnum(Gender, { message: ()=>{throw {status:401, message:'¡ERROR! The "Gender" parameter does not comply with the established data type >:('}}})
-    @Type(()=>String)
-    teacher_gender: string;
-
-    @Expose({ name: 'email' })
-    @IsEmail({}, { message: ()=>{throw {status:401, message:'¡ERROR! The "Email" parameter does not comply with the established data type >:('}}})
-    @Type(()=>String)
-    teacher_email: string;
 
     @Expose({ name: 'address' })
     @IsDefined({message: ()=>{throw {status:401, message:'¡ERROR! The "Address" parameter is required >:('}}})
@@ -62,24 +37,76 @@ export class teachers {
     @Type(()=>String)
     teacher_address : string;
 
+    @Expose({ name: 'phone' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Phone" parameter is required >:('}}})
+    @IsString({message: ()=>{throw {status:401, message: '¡ERROR! The "Phone" parameter does not comply with the established data type >:('}}})
+    @MaxLength(15,{message: ()=>{throw {status:401, message:'¡ERROR! The "Phone" parameter has exceeded the limit of characters >:('}}})
+    @Type(()=>String)
+    teacher_phone: string;
+
+    @Expose({ name: 'phone_type' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Phone_Type" parameter is required >:('}}})
+    @IsString({message: ()=>{throw {status:401, message: '¡ERROR! The "Phone_Type" parameter does not comply with the established data type >:('}}})
+    @MaxLength(15,{message: ()=>{throw {status:401, message:'¡ERROR! The "Phone_Type" parameter has exceeded the limit of characters >:('}}})
+    @Type(()=>String)
+    teacher_phone_type: string;
+
+    @Expose({ name: 'email' })
+    @IsEmail({}, { message: ()=>{throw {status:401, message:'¡ERROR! The "Email" parameter does not comply with the established data type >:('}}})
+    @Type(()=>String)
+    teacher_email: string;
+
+    @Expose({ name: 'email_type' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Email_Type" parameter is required >:('}}})
+    @IsString({message: ()=>{throw {status:401, message: '¡ERROR! The "Email_Type" parameter does not comply with the established data type >:('}}})
+    @MaxLength(15,{message: ()=>{throw {status:401, message:'¡ERROR! The "Email_Type" parameter has exceeded the limit of characters >:('}}})
+    @Type(()=>String)
+    teacher_email_type: string;
+
+    @Expose({ name: 'specialty_id' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Age" parameter is required >:('}}})
+    @IsInt({message: ()=>{throw {status:401, message: '¡ERROR! The "Age" parameter does not comply with the established data type >:('}}})
+    @Type(()=>Number)
+    teacher_specialty_id : number;
+
+    @Expose({ name: 'class_id' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Age" parameter is required >:('}}})
+    @IsInt({message: ()=>{throw {status:401, message: '¡ERROR! The "Age" parameter does not comply with the established data type >:('}}})
+    @Type(()=>Number)
+    teacher_class_id : number;
+
+    @Expose({ name: 'roll_id' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Age" parameter is required >:('}}})
+    @IsInt({message: ()=>{throw {status:401, message: '¡ERROR! The "Age" parameter does not comply with the established data type >:('}}})
+    @Type(()=>Number)
+    teacher_roll_id : number;
+
     constructor(
         teacher_id: number,
         teacher_name: string,
-        teacher_specialty: string,
-        teacher_phone: string,
+        teacher_gender: number,
         teacher_age: number,
-        teacher_gender: string,
+        teacher_address : string,
+        teacher_phone: string,
+        teacher_phone_type : string,
         teacher_email: string,
-        teacher_address : string
+        teacher_email_type: string,
+        teacher_specialty_id: number,
+        teacher_class_id: number,
+        teacher_roll_id: number
     ) {
         this.teacher_id = teacher_id;
         this.teacher_name = teacher_name;
-        this.teacher_specialty = teacher_specialty;
-        this.teacher_phone = teacher_phone;
-        this.teacher_age = teacher_age;
         this.teacher_gender = teacher_gender;
-        this.teacher_email = teacher_email;
+        this.teacher_age = teacher_age;
         this.teacher_address = teacher_address;
+        this.teacher_phone = teacher_phone;
+        this.teacher_phone_type = teacher_phone_type ;
+        this.teacher_email = teacher_email;
+        this.teacher_email_type = teacher_email_type;
+        this.teacher_specialty_id = teacher_specialty_id;
+        this.teacher_class_id = teacher_class_id;
+        this.teacher_roll_id = teacher_roll_id;
     }
 
 }
