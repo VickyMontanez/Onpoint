578fc225c9f71884dc205f02945c4239f6ed8466
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-07-23 12:28:07 -0500

    feat: ✨ Implement NEW DTO of students table
    Implement a new DTO method with typescript for students table

--- controllerDto/students.js
+++ controllerDto/students.js
@@ -8,23 +8,20 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 import { Expose, Type } from 'class-transformer';
-import { IsDefined, MaxLength, IsEnum, IsEmail, IsString, IsInt, Matches } from "class-validator";
-var Gender;
-(function (Gender) {
-    Gender["Male"] = "Male";
-    Gender["Female"] = "Female";
-    Gender["Other"] = "Other";
-})(Gender || (Gender = {}));
+import { IsDefined, MaxLength, IsEmail, IsString, IsInt, Matches } from "class-validator";
 export class students {
-    constructor(student_id, student_name, student_class_id, student_age, student_gender, student_phone, student_email, student_address) {
+    constructor(student_id, student_name, student_gender, student_age, student_address, student_phone, student_phone_type, student_email, student_email_type, student_class_id, student_roll_id) {
         this.student_id = student_id;
         this.student_name = student_name;
-        this.student_class_id = student_class_id;
-        this.student_age = student_age;
         this.student_gender = student_gender;
-        this.student_phone = student_phone;
-        this.student_email = student_email;
+        this.student_age = student_age;
         this.student_address = student_address;
+        this.student_phone = student_phone;
+        this.student_phone_type = student_phone_type;
+        this.student_email = student_email;
+        this.student_email_type = student_email_type;
+        this.student_class_id = student_class_id;
+        this.student_roll_id = student_roll_id;
     }
 }
 __decorate([
@@ -44,20 +41,12 @@ __decorate([
     __metadata("design:type", String)
 ], students.prototype, "student_name", void 0);
 __decorate([
-    Expose({ name: 'class_id' }),
-    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Class_Id" parameter is required >:(' }; } }),
-    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! The "Class_Id" parameter does not comply with the established data type >:(' }; } }),
+    Expose({ name: 'gender' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Gender" parameter is required >:(' }; } }),
+    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! The "Gender" parameter does not comply with the established data type >:(' }; } }),
     Type(() => Number),
     __metadata("design:type", Number)
-], students.prototype, "student_class_id", void 0);
-__decorate([
-    Expose({ name: 'phone' }),
-    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Phone" parameter is required >:(' }; } }),
-    IsString({ message: () => { throw { status: 401, message: '¡ERROR! The "Phone" parameter does not comply with the established data type >:(' }; } }),
-    MaxLength(15, { message: () => { throw { status: 401, message: '¡ERROR! The "Phone" parameter has exceeded the limit of characters >:(' }; } }),
-    Type(() => String),
-    __metadata("design:type", String)
-], students.prototype, "student_phone", void 0);
+], students.prototype, "student_gender", void 0);
 __decorate([
     Expose({ name: 'age' }),
     IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Age" parameter is required >:(' }; } }),
@@ -65,18 +54,6 @@ __decorate([
     Type(() => Number),
     __metadata("design:type", Number)
 ], students.prototype, "student_age", void 0);
-__decorate([
-    Expose({ name: 'gender' }),
-    IsEnum(Gender, { message: () => { throw { status: 401, message: '¡ERROR! The "Gender" parameter does not comply with the established data type >:(' }; } }),
-    Type(() => String),
-    __metadata("design:type", String)
-], students.prototype, "student_gender", void 0);
-__decorate([
-    Expose({ name: 'email' }),
-    IsEmail({}, { message: () => { throw { status: 401, message: '¡ERROR! The "Email" parameter does not comply with the established data type >:(' }; } }),
-    Type(() => String),
-    __metadata("design:type", String)
-], students.prototype, "student_email", void 0);
 __decorate([
     Expose({ name: 'address' }),
     IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Address" parameter is required >:(' }; } }),
@@ -86,3 +63,47 @@ __decorate([
     Type(() => String),
     __metadata("design:type", String)
 ], students.prototype, "student_address", void 0);
+__decorate([
+    Expose({ name: 'phone' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Phone" parameter is required >:(' }; } }),
+    IsString({ message: () => { throw { status: 401, message: '¡ERROR! The "Phone" parameter does not comply with the established data type >:(' }; } }),
+    MaxLength(15, { message: () => { throw { status: 401, message: '¡ERROR! The "Phone" parameter has exceeded the limit of characters >:(' }; } }),
+    Type(() => String),
+    __metadata("design:type", String)
+], students.prototype, "student_phone", void 0);
+__decorate([
+    Expose({ name: 'phone_type' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Phone_Type" parameter is required >:(' }; } }),
+    IsString({ message: () => { throw { status: 401, message: '¡ERROR! The "Phone_Type" parameter does not comply with the established data type >:(' }; } }),
+    MaxLength(15, { message: () => { throw { status: 401, message: '¡ERROR! The "Phone_Type" parameter has exceeded the limit of characters >:(' }; } }),
+    Type(() => String),
+    __metadata("design:type", String)
+], students.prototype, "student_phone_type", void 0);
+__decorate([
+    Expose({ name: 'email' }),
+    IsEmail({}, { message: () => { throw { status: 401, message: '¡ERROR! The "Email" parameter does not comply with the established data type >:(' }; } }),
+    Type(() => String),
+    __metadata("design:type", String)
+], students.prototype, "student_email", void 0);
+__decorate([
+    Expose({ name: 'email_type' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Email_Type" parameter is required >:(' }; } }),
+    IsString({ message: () => { throw { status: 401, message: '¡ERROR! The "Email_Type" parameter does not comply with the established data type >:(' }; } }),
+    MaxLength(15, { message: () => { throw { status: 401, message: '¡ERROR! The "Email_Type" parameter has exceeded the limit of characters >:(' }; } }),
+    Type(() => String),
+    __metadata("design:type", String)
+], students.prototype, "student_email_type", void 0);
+__decorate([
+    Expose({ name: 'class_id' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Class_Id" parameter is required >:(' }; } }),
+    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! The "Class_Id" parameter does not comply with the established data type >:(' }; } }),
+    Type(() => Number),
+    __metadata("design:type", Number)
+], students.prototype, "student_class_id", void 0);
+__decorate([
+    Expose({ name: 'roll_id' }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! The "Roll_Id" parameter is required >:(' }; } }),
+    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! The "Roll_Id" parameter does not comply with the established data type >:(' }; } }),
+    Type(() => Number),
+    __metadata("design:type", Number)
+], students.prototype, "student_roll_id", void 0);
--- dto/students.ts
+++ dto/students.ts
@@ -1,11 +1,5 @@
 import { Expose, Type} from 'class-transformer';
-import { IsDefined, MaxLength, IsEnum, IsEmail, IsString, IsInt, Matches} from "class-validator";
-
-enum Gender {
-    Male = 'Male',
-    Female = 'Female',
-    Other = 'Other',
-}
+import { IsDefined, MaxLength, IsEmail, IsString, IsInt, Matches} from "class-validator";
   
 export class students {
 
@@ -23,34 +17,17 @@ export class students {
     @Type(()=>String)
     student_name : string;
 
-    @Expose({ name: 'class_id' })
-    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Class_Id" parameter is required >:('}}})
-    @IsInt({message: ()=>{throw {status:401, message: '¡ERROR! The "Class_Id" parameter does not comply with the established data type >:('}}})
+    @Expose({ name: 'gender' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Gender" parameter is required >:('}}})
+    @IsInt({message: ()=>{throw {status:401, message: '¡ERROR! The "Gender" parameter does not comply with the established data type >:('}}})
     @Type(()=>Number)
-    student_class_id : number;
-
-    @Expose({ name: 'phone' })
-    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Phone" parameter is required >:('}}})
-    @IsString({message: ()=>{throw {status:401, message: '¡ERROR! The "Phone" parameter does not comply with the established data type >:('}}})
-    @MaxLength(15,{message: ()=>{throw {status:401, message:'¡ERROR! The "Phone" parameter has exceeded the limit of characters >:('}}})
-    @Type(()=>String)
-    student_phone: string;
+    student_gender: number;
 
     @Expose({ name: 'age' })
     @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Age" parameter is required >:('}}})
     @IsInt({message: ()=>{throw {status:401, message: '¡ERROR! The "Age" parameter does not comply with the established data type >:('}}})
     @Type(()=>Number)
     student_age: number;
-
-    @Expose({ name: 'gender' })
-    @IsEnum(Gender, { message: ()=>{throw {status:401, message:'¡ERROR! The "Gender" parameter does not comply with the established data type >:('}}})
-    @Type(()=>String)
-    student_gender: string;
-
-    @Expose({ name: 'email' })
-    @IsEmail({}, { message: ()=>{throw {status:401, message:'¡ERROR! The "Email" parameter does not comply with the established data type >:('}}})
-    @Type(()=>String)
-    student_email: string;
 
     @Expose({ name: 'address' })
     @IsDefined({message: ()=>{throw {status:401, message:'¡ERROR! The "Address" parameter is required >:('}}})
@@ -60,24 +37,68 @@ export class students {
     @Type(()=>String)
     student_address : string;
 
+    @Expose({ name: 'phone' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Phone" parameter is required >:('}}})
+    @IsString({message: ()=>{throw {status:401, message: '¡ERROR! The "Phone" parameter does not comply with the established data type >:('}}})
+    @MaxLength(15,{message: ()=>{throw {status:401, message:'¡ERROR! The "Phone" parameter has exceeded the limit of characters >:('}}})
+    @Type(()=>String)
+    student_phone: string;
+
+    @Expose({ name: 'phone_type' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Phone_Type" parameter is required >:('}}})
+    @IsString({message: ()=>{throw {status:401, message: '¡ERROR! The "Phone_Type" parameter does not comply with the established data type >:('}}})
+    @MaxLength(15,{message: ()=>{throw {status:401, message:'¡ERROR! The "Phone_Type" parameter has exceeded the limit of characters >:('}}})
+    @Type(()=>String)
+    student_phone_type: string;
+
+    @Expose({ name: 'email' })
+    @IsEmail({}, { message: ()=>{throw {status:401, message:'¡ERROR! The "Email" parameter does not comply with the established data type >:('}}})
+    @Type(()=>String)
+    student_email: string;
+
+    @Expose({ name: 'email_type' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Email_Type" parameter is required >:('}}})
+    @IsString({message: ()=>{throw {status:401, message: '¡ERROR! The "Email_Type" parameter does not comply with the established data type >:('}}})
+    @MaxLength(15,{message: ()=>{throw {status:401, message:'¡ERROR! The "Email_Type" parameter has exceeded the limit of characters >:('}}})
+    @Type(()=>String)
+    student_email_type: string;
+
+    @Expose({ name: 'class_id' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Class_Id" parameter is required >:('}}})
+    @IsInt({message: ()=>{throw {status:401, message: '¡ERROR! The "Class_Id" parameter does not comply with the established data type >:('}}})
+    @Type(()=>Number)
+    student_class_id : number;
+
+    @Expose({ name: 'roll_id' })
+    @IsDefined({message: ()=>{throw {status: 401, message: '¡ERROR! The "Roll_Id" parameter is required >:('}}})
+    @IsInt({message: ()=>{throw {status:401, message: '¡ERROR! The "Roll_Id" parameter does not comply with the established data type >:('}}})
+    @Type(()=>Number)
+    student_roll_id : number;
+
     constructor(
         student_id: number,
         student_name: string,
-        student_class_id: number,
+        student_gender: number,
         student_age: number,
-        student_gender: string,
+        student_address : string,
         student_phone: string,
+        student_phone_type : string,
         student_email: string,
-        student_address : string
+        student_email_type: string,
+        student_class_id: number,
+        student_roll_id: number
     ) {
         this.student_id = student_id;
         this.student_name = student_name;
-        this.student_class_id = student_class_id;
-        this.student_age = student_age;
         this.student_gender = student_gender;
-        this.student_phone = student_phone;
-        this.student_email = student_email;
+        this.student_age = student_age;
         this.student_address = student_address;
+        this.student_phone = student_phone;
+        this.student_phone_type = student_phone_type ;
+        this.student_email = student_email;
+        this.student_email_type = student_email_type;
+        this.student_class_id = student_class_id;
+        this.student_roll_id = student_roll_id;
     }
 
 }
